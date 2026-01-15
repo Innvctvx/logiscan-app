b86b441d866d26d868fdde5a715ee5efa435b510
Innvctvx <111038617+Innvctvx@users.noreply.github.com>
2026-01-15 17:08:56 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import { PackageCheck, ClipboardList, Settings, Truck, Code, ClipboardCheck, Loc
 
 // --- CONFIGURACIÓN ---
 // IMPORTANTE: Asegúrate que esta URL sea la de TU implementación "Aplicación Web" -> "Cualquier usuario"
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzUHQlg1j9bnLaDMSeCHvVtLq4UOUYoItqtXJCfm6mQjSFdtKTNKnsWUv0j09nTwn4G/exec"; 
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzlhw656fqVrapR-QDfyYwnoXhFvcTgNUltHXG2BxhBLHi97jsTaw8QbPqyBJmzje0V/exec"; 
 
 declare global {
   interface Window {
